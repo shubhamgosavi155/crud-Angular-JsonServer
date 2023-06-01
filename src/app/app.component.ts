3c733ad0e6412a8ad8baf1a4466eda4b5140cc67
shubhamgosavi155 <shubhamgosavi155@gmail.com>
2023-06-01 20:47:50 +0530

    first-Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { CommonService } from './common.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'crud-Angular';
+  allUser: any;
+  isEdit = false;
+  userObj: any = {
+    name: '',
+    mobile: '',
+    email: '',
+    password: '',
+    id: '',
+  };
+  constructor(private commonService: CommonService) {}
+
+  ngOnInit() {
+    this.getLatestUser();
+  }
+
+  addUser(myObj: any) {
+    console.log(myObj);
+    this.commonService.createUser(myObj).subscribe((res) => {
+      this.getLatestUser();
+    });
+  }
+  getLatestUser() {
+    this.commonService.getAllUser().subscribe((res) => {
+      this.allUser = res;
+    });
+  }
+
+  editUser(user: any) {
+    this.userObj = user;
+    this.isEdit = true;
+  }
+
+  deleteUser(user: any) {
+    this.commonService.deleteUser(user).subscribe(() => {
+      this.getLatestUser();
+    });
+  }
+
+  updateUser() {
+    this.isEdit = !this.isEdit;
+    this.commonService.updateUser(this.userObj).subscribe(() => {
+      this.getLatestUser();
+    });
+  }
 }
